b3777a25d734a6886210e23400abd2af5c3cb97f
Natali-Hovhannisyan <natalihovhannisyan00@gmail.com>
2023-05-16 19:14:31 +0400

    Made changes in setup

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = ['numpy', 'matplotlib']
 
@@ -31,14 +28,15 @@ setup(
     description="Python package for clustering",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='myclustering',
     name='myclustering',
     packages=find_packages(include=['myclustering', 'myclustering.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/Natali-Hovhannisyan/myclustering',
+    url='https://github.com/Natali-Hovhannisyan/DS233_Python_Package',
     version='0.1.0',
     zip_safe=False,
 )
